377fb1f81c67ee26e1c731a7926cf8b0f0aefb76
frank <603128423@qq.com>
2018-06-21 22:57:26 +0800

    add texture

---
+++ texture/texture.c
@@ -0,0 +1,123 @@
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <mycommon/loadShader.h>
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    int init = glfwInit();
+    if (!init) {
+        printf("glfw init error\n");
+        return 1;
+    }
+    glfwWindowHint(GL_MAJOR_VERSION, 4);
+    glfwWindowHint(GL_MINOR_VERSION, 6);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Texture", NULL, NULL);
+    if (NULL == window) {
+        printf("failed to create window\n");
+        glfwTerminate();
+        return 1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if(!gladLoadGLLoader(glfwGetProcAddress)) {
+        printf("failed to initialize glad\n");
+        return 1;
+    }
+
+    GLuint vshader, fshader;
+    int a = get_shader("./source/shader/texture.vert", GL_VERTEX_SHADER, &vshader);
+    if (a != 0) {
+        printf("failed to create vertex shader: %d\n", a);
+        return 1;
+    }
+    
+    a = get_shader("./source/shader/texture.frag", GL_FRAGMENT_SHADER, &fshader);
+    if (a != 0) {
+        printf("failed to create fragment shader: %d\n", a);
+        return 1;
+    }
+
+    GLuint* shaders = (GLuint*)malloc(2*sizeof(GLuint));
+    shaders[0] = vshader;
+    shaders[1] = fshader;
+    GLuint program;
+
+    a = get_program(2, shaders, &program);
+    if (a != 0) {
+        printf("create program error: %d\n", a);
+    }
+
+    float vertices[] = {
+        0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,  // 右上角
+        0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,  // 右下角
+        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,// 左下角
+        -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f   // 左上角
+    };
+
+    unsigned int indices[] = {
+        0, 1, 3,
+        1, 2, 3
+    };
+
+    GLuint vbo, vao, ibo;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    glGenBuffers(1, &ibo);
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8*sizeof(float), 3*sizeof(float));
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8*sizeof(float), 6*sizeof(float));
+    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
+    glEnableVertexAttribArray(2);
+
+    GLuint texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTextureParameteri(texture, GL_TEXTURE_WRAP_S, GL_REPEAT);  // 设置x轴纹理方式
+    glTextureParameteri(texture, GL_TEXTURE_WRAP_T, GL_REPEAT);  // 设置y轴纹理方式
+    glTextureParameteri(texture, GL_TEXTURE_MIN_FILTER, GL_LINEAR);  // 设置缩小时的纹理过滤
+    glTextureParameteri(texture, GL_TEXTURE_MAG_FILTER, GL_LINEAR);  // 设置放大时的纹理过滤
+
+    int width, height, channels;
+    unsigned char *data = stbi_load("./source/texture/container.jpg", &width, &height, &channels, 0);
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateTextureMipmap(GL_TEXTURE_2D);
+        stbi_image_free(data);
+    } else {
+        printf("failed to load texture\n");
+        return 1;
+    }
+
+    while(!glfwWindowShouldClose(window)) {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(program);
+
+        glBindVertexArray(vao);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+
+    }
+
+    glDeleteVertexArrays(1, &vao);
+    glDeleteBuffers(1, &vbo);
+    glDeleteBuffers(1, &ibo);
+
+    glfwTerminate();
+    return 0;
+    
+}
