b32d90718dc38354acba25df4235694362910cbc
frank <603128423@qq.com>
2018-08-05 15:14:54 +0800

    load

--- objimp/load.c
+++ objimp/load.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "load.h"
+// #include "load.h"
 #include "model.h"
 #include <mycommon/strutil.h>
 
@@ -39,8 +39,8 @@ typedef struct LineNode
 {
     char* line;  // 如果 !is_continued 以 \0 结尾 否则以 \ 结尾
     int is_continued; // 1: y   0: n
-    LN* prev;
-    LN* next;
+    struct LineNode* prev;
+    struct LineNode* next;
 } LN;
 
 void free_node(LN* node)
@@ -59,6 +59,46 @@ void free_node_list(LN* head)
     }
 }
 
+// remove \r
+char* remove_return(char* s)
+{
+    int count = 0;
+    int len = strlen(s);
+    for (int i=0; i < len; i++)
+    {
+        if (s[i] == '\r')
+        {
+            count++;
+        }
+    }
+    if (count)
+    {
+        char* new_s = malloc((len - count + 1)*sizeof(char));
+        int c = 0;
+        for (int i = 0; i < len; i++)
+        {
+            if ('\r' != s[i])
+            {
+                new_s[c++] = s[i];
+            }
+        }
+        new_s[c] = '\0';
+        free(s);
+        return new_s;
+    }
+    return s;
+}
+
+char* remove_new_line(char* s)
+{
+    int len = strlen(s);
+    if ('\n' == s[len-1])
+    {
+        s[len-1] = '\0';
+    }
+    return s;
+}
+
 /**
  * 读取文件 返回链表
  */ 
@@ -72,10 +112,12 @@ LN* read_file(char* filename)
     while (1)    
     {
         char* line = NULL;
-        int len;
+        size_t len;
         int read = getline(&line, &len, fp);
         if (read < 0)
             break;
+        line = remove_return(line);
+        line = remove_new_line(line);
         if (!is_comment_or_empty(line))
         {
             LN* n = (LN*)malloc(sizeof(LN));
@@ -90,14 +132,15 @@ LN* read_file(char* filename)
                 }
                 return NULL;
             }
-            if (is_continued(line, read))
+            if (is_continued(line, strlen(line)))
             {
+                line[strlen(line)] = '\0';
                 n->line = line;
                 n->is_continued = 1;
             }
             else 
             {
-                line[read] = '\0';
+                line[strlen(line)] = '\0';
                 n->line = line;
                 n->is_continued = 0;
             }
@@ -137,7 +180,6 @@ int merge(LN* head)
                 LN* next = node->next;
                 int nn_len = strlen(next->line);
                 node->is_continued = next->is_continued;
-                node->line[n_len-1] = ' ';
                 char* tmp = (char*)realloc(node->line, (n_len+nn_len)*sizeof(char));
                 if (!tmp)
                     return 0;
@@ -149,11 +191,14 @@ int merge(LN* head)
                 
                 for (int i = 0; i < nn_len; i++)
                 {
-                    node->line[n_len+i] = next->line[i];
+                    node->line[n_len-1+i] = next->line[i];
                 }
-                
+                node->line[n_len+nn_len-1] = '\0';
                 node->next = next->next;
-                next->next->prev = node;
+                if (next->next)
+                {
+                    next->next->prev = node;
+                }
                 free_node(next);
             }
            
@@ -167,11 +212,27 @@ int merge(LN* head)
     return 1;
 }
 
+void tranverse(LN* head)
+{
+    LN* node = head;
+    while (node)
+    {
+        printf("%d---%s\n", strlen(node->line), node->line);
+        node = node->next;
+    }
+}
+
+void test()
+{
+    LN* head = read_file("./text.txt");
+    tranverse(head);
+    printf("======================\n");
+    merge(head);
+    tranverse(head);
+}
+
 int main() 
 {
-    char* s = "";
-    printf("%d\n", is_comment_or_empty(s));
-    s = NULL;
-    printf("%d\n", is_comment_or_empty(s));
+    test();
     return 0;
 }
